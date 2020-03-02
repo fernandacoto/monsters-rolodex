5445a27b28fedbda7f290f9bafea9b75280fe0f7
Fernanda Coto <fernanda.coto@lognllc.com>
2020-03-02 13:23:53 -0600

    Updates

---
+++ precache-manifest.eb8ae0561c4d0a84a5672b80a0fcefa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012adf36a569198ed933a53b462f72ba",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "6426e37f3602da59de49",
+    "url": "/monsters-rolodex/static/css/main.2ad98df5.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6426e37f3602da59de49",
+    "url": "/monsters-rolodex/static/js/main.cd438a47.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
